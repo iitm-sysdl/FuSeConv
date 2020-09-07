c42c002bda92db9bf30b81054744a4061de525ef
surya <surya00060@gmail.com>
2020-09-07 16:38:09 +0530

    Added ResNet Hybrid

--- latencytest.py
+++ latencytest.py
@@ -1,31 +1,36 @@
 import torch
 from models import *
 from utils import *
-
-net1 = ResNet50(1000)
-net2 = ResNet50Friendly(1000)
-net3 = ResNet50Friendly2(1000)
-net4 = ResNet50Friendly3(1000)
-net5 = ResNet50Friendly4(1000)
-net6 = ResNet50Friendly5(1000)
-net7 = ResNet50Friendly6(1000)
+import mobilenetv3fusedHybrid as mv3hybrid
+import resnetfusedHybrid as resnetgyhbrid
 
 x = torch.randn([1,3,224,224])
-latb = getModelLatency(net1, x)
-lat1 = getModelLatency(net2, x)
-lat2 = getModelLatency(net3, x)
-lat3 = getModelLatency(net4, x)
-lat4 = getModelLatency(net5, x)
-lat5 = getModelLatency(net6, x)
-lat6 = getModelLatency(net7, x)
+###########MobileNetV3##########
+print("MobileNet-V3")
+for size in ['small', 'large']:
+    net = MobileNetV3(size, 1000)
+    latb = getModelLatency(net, x)
+    net = MobileNetV3Friendly(size, 1000)
+    latf = getModelLatency(net, x)
+    net = MobileNetV3Friendly2(size, 1000)
+    latf2 = getModelLatency(net, x)
+    net = mv3hybrid.MobileNetV3Friendly(size, 1000)
+    lath = getModelLatency(net, x)
+    net = mv3hybrid.MobileNetV3Friendly2(size, 1000)
+    lath2 = getModelLatency(net, x)
+    print(latb/latf, latb/latf2, latb/lath, latb/lath2)
+#########
+print('ResNet')
+net = ResNet50(1000)
+latb = getModelLatency(net, x)
+net = ResNet50Friendly(1000)
+latf = getModelLatency(net, x)
+net = ResNet50Friendly2(1000)
+latf2 = getModelLatency(net, x)
+net = resnetgyhbrid.ResNet50Fused1(1000)
+lath = getModelLatency(net, x)
+net = resnetgyhbrid.ResNet50Fused2(1000)
+lath2 = getModelLatency(net, x)
+print(latb/latf, latb/latf2, latb/lath, latb/lath2)
+####
 
-print("ResNet SpeedUp")
-print(latb/lat1, latb/lat2, latb/lat3, latb/lat4, latb/lat5, latb/lat6)
-
-print("MobileNet Speedup")
-net1 = MobileNetV3('large', 1000)
-net2 = MobileNetV3Friendly('large', 1000)
-x = torch.randn([1,3,224,224])
-latb = getModelLatency(net1, x)
-lat1 = getModelLatency(net2, x)
-print(latb/lat1)
