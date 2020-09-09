6bfb2fe45ee41bdae360868bdd6cfa12f22f43bd
surya <surya00060@gmail.com>
2020-09-09 19:54:24 +0530

    ADded MNASNet

--- latencytest.py
+++ latencytest.py
@@ -9,10 +9,25 @@ import mobilenetv2fusedHybrid as mv2hybrid
 import mobilenetv1hybrid as mv1hybrid
 
 x = torch.randn([1,3,224,224])
-mode = 'analytical'
-# model ='scale-sim'
-arraySize = 16
+# mode = 'analytical'
+mode ='scale-sim'
+arraySize = 64
 start_time = time.time()
+#####MnasNet
+print("--- %s seconds ---" % (time.time() - start_time))
+print('MnasNet')
+net = MnasNet(1000)
+latb = getModelLatency(net, x, mode, arraySize)
+net = MnasNetFriendly(1000)
+latf = getModelLatency(net, x, mode, arraySize)
+net = MnasNetFriendly2(1000)
+latf2 = getModelLatency(net, x, mode, arraySize)
+# net = mv2hybrid.MobileNetV2Friendly(1000)
+# lath = getModelLatency(net, x, mode, arraySize)
+# net = mv2hybrid.MobileNetV2Friendly2(1000)
+# lath2 = getModelLatency(net, x, mode, arraySize)
+print(latb/latf, latb/latf2)#, latb/lath, latb/lath2)
+exit()
 ###########MobileNetV3##########
 print("MobileNet-V3")
 for size in ['small', 'large']:
@@ -27,20 +42,6 @@ for size in ['small', 'large']:
     net = mv3hybrid.MobileNetV3Friendly2(size, 1000)
     lath2 = getModelLatency(net, x, mode, arraySize)
     print(latb/latf, latb/latf2, latb/lath, latb/lath2)
-#########
-print("--- %s seconds ---" % (time.time() - start_time))
-print('ResNet')
-net = ResNet50(1000)
-latb = getModelLatency(net, x, mode, arraySize)
-net = ResNet50Friendly(1000)
-latf = getModelLatency(net, x, mode, arraySize)
-net = ResNet50Friendly2(1000)
-latf2 = getModelLatency(net, x, mode, arraySize)
-net = resnetgyhbrid.ResNet50Fused1(1000)
-lath = getModelLatency(net, x, mode, arraySize)
-net = resnetgyhbrid.ResNet50Fused2(1000)
-lath2 = getModelLatency(net, x, mode, arraySize)
-print(latb/latf, latb/latf2, latb/lath, latb/lath2)
 ####
 print("--- %s seconds ---" % (time.time() - start_time))
 print('MobileNet-V2')
@@ -67,6 +68,20 @@ latf2 = getModelLatency(net, x, mode, arraySize)
 net = mv1hybrid.MobileNetV1Friendly(1000)
 lath = getModelLatency(net, x, mode, arraySize)
 net = mv1hybrid.MobileNetV1Friendly2(1000)
+lath2 = getModelLatency(net, x, mode, arraySize)
+print(latb/latf, latb/latf2, latb/lath, latb/lath2)
+#########
+print("--- %s seconds ---" % (time.time() - start_time))
+print('ResNet')
+net = ResNet50(1000)
+latb = getModelLatency(net, x, mode, arraySize)
+net = ResNet50Friendly(1000)
+latf = getModelLatency(net, x, mode, arraySize)
+net = ResNet50Friendly2(1000)
+latf2 = getModelLatency(net, x, mode, arraySize)
+net = resnetgyhbrid.ResNet50Fused1(1000)
+lath = getModelLatency(net, x, mode, arraySize)
+net = resnetgyhbrid.ResNet50Fused2(1000)
 lath2 = getModelLatency(net, x, mode, arraySize)
 print(latb/latf, latb/latf2, latb/lath, latb/lath2)
 ##### Breakdown Analysis
--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,5 @@ from .vgg import *
 from .mobilenetv1 import *
 from .mobilenetv2 import *
 from .mobilenetv3 import *
+from .mnasnet import *
 from .squeezenet import *
